fea5fe54486f0ef32f9cc970c738f52b84bb8cb5
paula.mas.pascual <paula.mas.pascual@estudiantat.upc.edu>
2026-04-25 13:15:13 +0200

    full implementation

--- AgentZon/protocols/cerca.py
+++ AgentZon/protocols/cerca.py
@@ -1,5 +1,4 @@
-from typing import List
-from typing import Optional
+from typing import List, Optional
 
 # definició de producte per als missatges
 class ProducteModel:
@@ -12,6 +11,8 @@ class ProducteModel:
         self.categ = categ          # categoria per filtrar
         self.marca = marca          # marca del producte
         self.pes = pes              # pes del producte
+    
+# ---------------------------------------------------------------------------------------
 
 # definició del missatge de Cerca
 class ResultatCerca:
@@ -21,6 +22,7 @@ class ResultatCerca:
         self.id = id                                # identificador únic
         self.total = len(llista_productes)          # comptador de resultats de la cerca
 
+# definició de la petició de cerca per part de l'usuari (no tinc clar si mirar q algo no sigui buit o nomes passar-vos les coses plenes)
 class PeticioCerca:
     def __init__( self, id: Optional[str], text: Optional[str] = "", categ: Optional[str] = None, marca: Optional[str] = None,
     preu_min: Optional[float] = None, preu_max: Optional[float] = None):
@@ -32,9 +34,31 @@ class PeticioCerca:
         self.preu_min = preu_min          # preu minim
         self.preu_max = preu_max          # preu maxim
 
-        if (
-            self.preu_min is not None
-            and self.preu_max is not None
-            and self.preu_min > self.preu_max
-        ):
-            raise ValueError("El preu mínim no pot ser mes gran que el preu màxim.")
+        if self.preu_min and self.preu_max and self.preu_min > self.preu_max:
+            raise ValueError("El preu mínim no pot ser més gran que el màxim.")
+        
+    # comprova que un producte compleixi les condicions de la cerca de l'usuari
+    def producte_compleix_filtres(self, p: ProducteModel) -> bool:
+        if self.text and not (self.text in p.nom.lower() or self.text in p.descr.lower()):
+            return False
+        
+        if self.categ and p.categ != self.categ:
+            return False
+            
+        if self.marca and p.marca != self.marca:
+            return False
+            
+        if self.preu_min is not None and p.preu < self.preu_min:
+            return False
+        
+        if self.preu_max is not None and p.preu > self.preu_max:
+            return False
+            
+        return True
+    
+# -----------------------------------------------------------------
+
+# func que retorna els productes que compleixen es condicions
+def cercar_en_base_de_dades(peticio: PeticioCerca, inventari_complet: List[ProducteModel]) -> ResultatCerca:
+    resultats = [p for p in inventari_complet if peticio.producte_compleix_filtres(p)]
+    return ResultatCerca(resultats, peticio.id)
--- AgentZon/utils/interface.py
+++ AgentZon/utils/interface.py
@@ -1,4 +1,30 @@
-from protocols.cerca import MostrarCerca
+from protocols.cerca import MostrarCerca, PeticioCerca
+from protocols.compra import PeticioInfoUsuari, InformacioUsuari, PeticioCompra
+from typing import Optional
+import uuid
+
+# ------------- cerca -----------------------------------------
+
+def demanar_cerca_usuari() -> PeticioCerca:
+    print("\n--- Cercador de productes ---")
+    text = input("Què busques? (nom/descripció): ")
+    categ = input("Categoria (opcional): ")
+    marca = input("Marca (opcional): ")
+    
+    p_min = input("Preu mínim (opcional): ")
+    p_max = input("Preu màxim (opcional): ")
+    
+    p_min = float(p_min) if p_min else None
+    p_max = float(p_max) if p_max else None
+    
+    return PeticioCerca(
+        id=str(uuid.uuid4()), 
+        text=text, 
+        categ=categ if categ else None, 
+        marca=marca if marca else None,
+        preu_min=p_min,
+        preu_max=p_max
+    )
 
 def mostrar_resultats_cerca(answ: MostrarCerca):
     print(f"\n Resultats de la cerca ID: {answ.id}")
@@ -11,3 +37,44 @@ def mostrar_resultats_cerca(answ: MostrarCerca):
             print(f"[{p.id}] {p.nom.upper()}")
             print(f"Preu: {p.preu}€")
             print(f"Descripció: {p.descr}")
+
+# ------------- compra -----------------------------------------
+def demanar_seleccio_compra(userid: str, llista_mostrada: list) -> Optional[PeticioCompra]:
+
+    print("\n--- Finalitzar compra ---")
+    ids_input = input("Escriu els IDs dels productes que vols comprar (separats per coma): ")
+    llista_ids = [i.strip() for i in ids_input.split(",")]
+    
+    seleccionats = [p for p in llista_mostrada if p.id in llista_ids]
+    
+    if not seleccionats:
+        print("No has seleccionat cap producte vàlid.")
+        return None
+        
+    return PeticioCompra(userid=userid, llista_productes=seleccionats)
+
+def demanar_dades_enviament(peticio: PeticioInfoUsuari) -> Optional[InformacioUsuari]:
+
+    print(f"\n--- Formulari de compra per a l'usuari: {peticio.userid} ---")
+    print(f"L'agent necessita: {', '.join(peticio.camps_requerits)}")
+    
+    adreça = input("Introdueix l'adreça d'enviament: ").strip()
+    if not adreça:
+        print("L'adreça d'enviament no pot ser buida.")
+        return None
+    
+    try:
+        prioritat = int(input("Prioritat (1:Express, 2:Normal): "))
+        if prioritat not in [1, 2]:
+            print("La prioritat ha de ser 1 (Express) o 2 (Normal).")
+            return None
+    except ValueError:
+        print("La prioritat ha de ser un número.")
+        return None
+    
+    metodepagament = input("Mètode de pagament: ").strip()
+    if not metodepagament:
+        print("El mètode de pagament no pot ser buit.")
+        return None
+    
+    return InformacioUsuari(peticio.userid, adreça, prioritat, metodepagament)
