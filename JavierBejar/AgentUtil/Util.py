5ebf175a0bf070188a59236d6c61dcf35c5b2764
angelabuxo <angiangeleta2@gmail.com>
2026-05-03 18:16:32 +0200

    carpeta amb plantilles del Javier

---
+++ JavierBejar/AgentUtil/Util.py
@@ -0,0 +1,31 @@
+"""
+.. module:: Util.py
+
+Util.py
+******
+
+:Description: Util.py
+
+    Different Auxiliary functions used for different purposes
+
+:Authors:
+    bejar
+
+:Version:
+
+:Date:  23/02/2021 ###
+"""
+
+import socket
+
+from pif import get_public_ip
+
+__author__ = "bejar"
+
+
+def gethostname():
+    try:
+        return socket.gethostbyaddr(get_public_ip())[0]
+    except Exception as e:
+        print("Cannot get public IP:", str(e))
+        return socket.gethostname()
---
+++ JavierBejar/Examples/Distributed/DistributedSolverClosed/Util.py
@@ -0,0 +1,30 @@
+"""
+.. module:: Util.py
+
+Util.py
+******
+
+:Description: Util.py
+
+    Different Auxiliary functions used for different purposes
+
+:Authors:
+    bejar
+
+:Version:
+
+:Date:  23/02/2021
+"""
+
+import socket
+from pif import get_public_ip
+
+__author__ = "bejar"
+
+
+def gethostname():
+    try:
+        return socket.gethostbyaddr(get_public_ip())[0]
+    except Exception as e:
+        print(f"Could not get public hostname: {e}")
+        return socket.gethostname()
